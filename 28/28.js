9a6643891d87c9d3897fc25105adb08568478e54
Florian Cargoët <florian.cargoet@gmail.com>
2013-10-02 23:53:31 +0200

    28

---
+++ 28/28.js
@@ -0,0 +1,27 @@
+/*
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+*/
+
+var sum = 1;
+var i = 1;
+var size = 1001;
+
+for (var increment = 2; increment < size + 1; increment += 2) {
+    // one turn
+    for (var j = 0; j < 4; j++) {
+        i += increment;
+        sum += i;
+    }
+}
+
+console.log(sum);
